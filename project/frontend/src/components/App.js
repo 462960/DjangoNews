b61ea49448d0768f84ed3abb095d3730a9b05a72
Shitikov Kirill <sh.kiruh@gmail.com>
2018-09-29 22:59:18 +0300

    Add form

--- project/frontend/src/components/App.js
+++ project/frontend/src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
 import Table from "./Table";
+import Form from "./Form";
 
 const App = () => (
-  <DataProvider endpoint="api/news/"
-                render={data => <Table data={data} />} />
+    <React.Fragment>
+        <DataProvider endpoint="api/news/"
+                      render={data => <Table data={data}/>}/>
+        <Form endpoint="api/lead/"/>
+    </React.Fragment>
 );
-
 const wrapper = document.getElementById("app");
-
-wrapper ? ReactDOM.render(<App />, wrapper) : null;
+wrapper ? ReactDOM.render(<App/>, wrapper) : null;
